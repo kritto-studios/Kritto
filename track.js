24fdd8bb0b5d731f62737cad5c3e12ecbd8ee7b5
Madeeha <141005402+kritto-studios@users.noreply.github.com>
2026-03-18 15:34:48 +0600

    Create track.js

---
+++ track.js
@@ -0,0 +1,41 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+/* CONFIG */
+const firebaseConfig = {
+  apiKey: "AIzaSyB2V7uSRF88EEa_7ca8M2L6bnmYMl6zLBE",
+  authDomain: "kritto-7c55b.firebaseapp.com",
+  projectId: "kritto-7c55b",
+  storageBucket: "kritto-7c55b.firebasestorage.app",
+  messagingSenderId: "1091669122505",
+  appId: "1:1091669122505:web:c3b83baed9db2a5ce8dc18",
+  measurementId: "G-CL3SMLSLVN"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+window.track = async function () {
+  const input = document.getElementById("trackInput").value;
+  const result = document.getElementById("result");
+
+  const querySnapshot = await getDocs(collection(db, "cards"));
+
+  let found = false;
+
+  querySnapshot.forEach((doc) => {
+    const data = doc.data();
+
+    if (data.trackingId === input) {
+      result.innerHTML = `
+        <p><b>Process:</b> ${data.status}</p>
+      `;
+      found = true;
+    }
+  });
+
+  if (!found) {
+    result.innerHTML = "Tracking ID not found.";
+  }
+};
