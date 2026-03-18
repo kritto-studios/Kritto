016c8a20b8e54e7ad785ed3a9f53555a3719248c
Madeeha <141005402+kritto-studios@users.noreply.github.com>
2026-03-18 15:26:32 +0600

    Update cards.js

--- cards.js
+++ cards.js
@@ -1,17 +1,18 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getFirestore, addDoc, collection } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
-/* FIREBASE CONFIG */
+/* CONFIG */
 const firebaseConfig = {
   apiKey: "AIzaSyB2V7uSRF88EEa_7ca8M2L6bnmYMl6zLBE",
   authDomain: "kritto-7c55b.firebaseapp.com",
   projectId: "kritto-7c55b",
   storageBucket: "kritto-7c55b.firebasestorage.app",
   messagingSenderId: "1091669122505",
-  appId: "1:1091669122505:web:c3b83baed9db2a5ce8dc18"
+  appId: "1:1091669122505:web:c3b83baed9db2a5ce8dc18",
+  measurementId: "G-CL3SMLSLVN"
 };
 
-/* INITIALIZE */
+
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
@@ -32,14 +33,14 @@ form.addEventListener("submit", async (e) => {
       idea,
       imageUrl,
       delivery,
+      status: "Received", // Kritto system starts here
       createdAt: new Date()
     });
 
-    alert("Submitted successfully");
+    alert("Your request has been received");
     form.reset();
 
   } catch (error) {
     alert("Error: " + error.message);
-    console.error(error);
   }
 });
